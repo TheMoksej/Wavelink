359a988463bbfb6fbfbde0888d697c84b99ac68a
Moksej <58531286+TheMoksej@users.noreply.github.com>
2021-02-23 20:02:52 +0100

    fix some stuff, remove eqs

--- wavelink/eqs.py
+++ wavelink/eqs.py
@@ -21,107 +21,6 @@ import collections
 from typing import Optional
 
 
-class Equalizer:
-    """Class representing an Equalizer filter.
-    Parameters
-    ------------
-    levels: List[Tuple[int, float]]
-        A list of tuple pairs containing a band int and gain float.
-    name: str
-        An Optional string to name this Equalizer. Defaults to 'CustomEqualizer'
-    Attributes
-    ------------
-    eq: list
-        A list of {'band': int, 'gain': float}
-    raw: list
-        A list of tuple pairs containing a band int and gain float.
-    """
-    def __init__(self, *, levels: list, name: str = 'CustomEqualizer'):
-
-        self.eq = self._factory(levels)
-        self.raw = levels
-
-        self._name = name
-
-    def __str__(self):
-        return self._name
-
-    def __repr__(self):
-        return f'<wavelink.filter.Equalizer: {self._name}, Raw: {self.eq}>'
-
-    @property
-    def name(self):
-        """The Equalizers friendly name."""
-        return self._name
-
-    @staticmethod
-    def _factory(levels: list):
-        _dict = collections.defaultdict(int)
-
-        _dict.update(levels)
-        _dict = [{"band": i, "gain": _dict[i]} for i in range(15)]
-
-        return _dict
-
-    @classmethod
-    def build(cls, *, levels: list, name: str = 'CustomEqualizer'):
-        """Build a custom Equalizer class with the provided levels.
-        Parameters
-        ------------
-        levels: List[Tuple[int, float]]
-            A list of tuple pairs containing a band int and gain float.
-        name: str
-            An Optional string to name this Equalizer. Defaults to 'CustomEqualizer'
-        """
-        return cls(levels=levels, name=name)
-
-    @classmethod
-    def flat(cls):
-        """Flat Equalizer.
-        Resets your EQ to Flat.
-        """
-        levels = [(0, .0), (1, .0), (2, .0), (3, .0), (4, .0),
-                  (5, .0), (6, .0), (7, .0), (8, .0), (9, .0),
-                  (10, .0), (11, .0), (12, .0), (13, .0), (14, .0)]
-
-        return cls(levels=levels, name='Flat')
-
-    @classmethod
-    def boost(cls):
-        """Boost Equalizer.
-        This equalizer emphasizes Punchy Bass and Crisp Mid-High tones.
-        Not suitable for tracks with Deep/Low Bass.
-        """
-        levels = [(0, -0.075), (1, .125), (2, .125), (3, .1), (4, .1),
-                  (5, .05), (6, 0.075), (7, .0), (8, .0), (9, .0),
-                  (10, .0), (11, .0), (12, .125), (13, .15), (14, .05)]
-
-        return cls(levels=levels, name='Boost')
-
-    @classmethod
-    def metal(cls):
-        """Experimental Metal/Rock Equalizer.
-        Expect clipping on Bassy songs.
-        """
-        levels = [(0, .0), (1, .1), (2, .1), (3, .15), (4, .13),
-                  (5, .1), (6, .0), (7, .125), (8, .175), (9, .175),
-                  (10, .125), (11, .125), (12, .1), (13, .075), (14, .0)]
-
-        return cls(levels=levels, name='Metal')
-
-    @classmethod
-    def piano(cls):
-        """Piano Equalizer.
-        Suitable for Piano tracks, or tacks with an emphasis on Female Vocals.
-        Could also be used as a Bass Cutoff.
-        """
-        levels = [(0, -0.25), (1, -0.25), (2, -0.125), (3, 0.0),
-                  (4, 0.25), (5, 0.25), (6, 0.0), (7, -0.25), (8, -0.25),
-                  (9, 0.0), (10, 0.0), (11, 0.5), (12, 0.25), (13, -0.025)]
-
-        return cls(levels=levels, name='Piano')
-
-
 class Karaoke:
     """Class representing a Karaoke filter.
     .. versionadded:: 0.10.0
@@ -154,7 +53,7 @@ class Karaoke:
 
     @property
     def payload(self) -> dict:
-        return {'level': self.level, 'mono_level': self.mono_level, 'filter_band': self.filter_band, 'filter_width': self.filter_width}
+        return {'karaoke': {'level': self.level, 'mono_level': self.mono_level, 'filter_band': self.filter_band, 'filter_width': self.filter_width}}
 
 
 class Timescale:
@@ -230,7 +129,7 @@ class Tremolo:
 
     @property
     def payload(self) -> dict:
-        return {'frequency': self.frequency, 'depth': self.depth}
+        return {'tremolo': {'frequency': self.frequency, 'depth': self.depth}}
 
 
 class Vibrato:
@@ -269,7 +168,7 @@ class Vibrato:
 
     @property
     def payload(self) -> dict:
-        return {'frequency': self.frequency, 'depth': self.depth}
+        return {'vibrato': {'frequency': self.frequency, 'depth': self.depth}}
 
 
 class Filter:
